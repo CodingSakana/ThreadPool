e8f7118192e5f6e41f62cc108cafee3a33baf325
SSSakana <SSSakana.Zhou@gmali.com>
2025-05-09 20:25:39 +0800

    加一些注释

--- ThreadPool.h
+++ ThreadPool.h
@@ -16,13 +16,13 @@ public:
     ThreadPool(size_t);
     template<class F, class... Args>
     auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+        -> std::future<std::invoke_result_t<F, Args...>>;
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
     // the task queue
-    std::queue< std::function<void()> > tasks;
+    std::queue< std::function<void()> > tasks;  // usage: std::function<返回类型(参数类型1, 参数类型2, ...)>
     
     // synchronization
     std::mutex queue_mutex;
@@ -45,14 +45,14 @@ inline ThreadPool::ThreadPool(size_t threads)
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
                         this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                            [this]{ return this->stop || !this->tasks.empty(); }); // 等待直到线程池有任务
                         if(this->stop && this->tasks.empty())
                             return;
-                        task = std::move(this->tasks.front());
+                        task = std::move(this->tasks.front());  // 取任务
                         this->tasks.pop();
                     }
 
-                    task();
+                    task(); // 执行任务
                 }
             }
         );
@@ -61,12 +61,14 @@ inline ThreadPool::ThreadPool(size_t threads)
 // add new work item to the pool
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
+    -> std::future<std::invoke_result_t<F, Args...>>    // 相当于 C++11::  typename std::result_of<F(Args...)>::type                             
+    {                                                   // 编译期推导F(Args...)的返回类型 注意不是f(args...) 这个是运行期的对象参数
+    using return_type = std::invoke_result_t<F, Args>;
+    // C++11::   using return_type = typename std::result_of<F(Args...)>::type;
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+                                                                        // packaged_task 封装函数相当于把参数解决掉了
+    auto task = std::make_shared< std::packaged_task<return_type()> >(  // 类似于是 function<return_type()> 只不过可以用 get_future()
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)      // 初始化 packaged_task
         );
         
     std::future<return_type> res = task->get_future();
@@ -77,7 +79,8 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
         if(stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task](){ (*task)(); });
+        tasks.emplace([task](){ (*task)(); });  // 这个 lambda式 符合 void() 的签名 所以可以加入 std::queue< std::function<void()> >
+                                                // lambda式 为了解决 packaged_task<return_type()> 的返回值
     }
     condition.notify_one();
     return res;
